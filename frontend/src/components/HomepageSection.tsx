d57b2173c1121d8718b328ba8bf6be0a84da5fea
nszp <nathanielszp@gmail.com>
2025-06-19 23:48:31 -0400

    forgot the ref

--- frontend/src/components/HomepageSection.tsx
+++ frontend/src/components/HomepageSection.tsx
@@ -1,15 +1,19 @@
-import { Flex, Text } from "@mantine/core";
+import { Flex, type PolymorphicRef, Text } from "@mantine/core";
+import type * as React from "react";
+import type { Ref } from "react";
 
 export function HomepageSection({
   children,
   name,
-}: React.PropsWithChildren<{ name?: string }>) {
+  ref,
+}: React.PropsWithChildren<{ name?: string; ref?: Ref<HTMLDivElement> }>) {
   return (
     <Flex
       justify="center"
       align="center"
       mb="md"
       direction="column"
+      ref={ref}
       sx={(_, u) => ({
         [u.smallerThan("sm")]: {
           width: "100%",
