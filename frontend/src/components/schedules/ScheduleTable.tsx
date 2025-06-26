252e53b30c74b868aaf74786871e8085feb38f67
N Szpunar <nathanielszp@gmail.com>
2025-06-26 00:36:02 -0500

    align team names in schedule table

--- frontend/src/components/schedules/ScheduleTable.tsx
+++ frontend/src/components/schedules/ScheduleTable.tsx
@@ -17,7 +17,8 @@ import {
   MonitorPlayIcon,
   SheetIcon,
 } from "lucide-react";
-import { DataTable } from "mantine-datatable";
+import { DataTable, type DataTableColumn } from "mantine-datatable";
+import { useMemo } from "react";
 
 type Props = {
   statGroupName?: string;
@@ -41,6 +42,8 @@ type QuizWithoutTime = {
   includeTime: false;
 };
 
+type ArrayElement<T> = T extends (infer U)[] ? U : never;
+
 export default function ScheduleTable({
   quizzes,
   statGroupName,
@@ -49,223 +52,413 @@ export default function ScheduleTable({
   includeTime = true,
   showCurrentQuestionColumn = false,
 }: Props) {
-  return (
-    <Box mb="md" mih="50vh">
-      <DataTable
-        columns={[
-          {
-            accessor: "icon",
-            title: "",
-            textAlign: "center",
-            width: "50px",
-            render: (quiz) => {
-              if (!quiz.completed && !quiz.inProgress) {
-                // Scheduled quiz
-                return (
-                  <CalendarIcon
-                    color={theme.colors.gray[6]}
-                    size={24}
-                    style={{ marginBottom: "-5px" }}
-                  />
-                );
-              }
-              if (quiz.inProgress) {
-                // Quiz in progress
-                return (
-                  <Text
-                    component={Link}
-                    to={roomStreamRoute.to}
-                    // @ts-ignore (type safety unfortunately doesn't work with polymorphic links)
-                    params={{ roomName: quiz.room }}
-                  >
-                    <MonitorPlayIcon
-                      color={theme.colors.blue[4]}
-                      size={24}
-                      style={{
-                        marginBottom: "-6px",
-                      }}
-                    />
-                  </Text>
-                );
-              }
-              if (quiz.completed) {
-                // Completed quiz
-                return (
-                  <CheckIcon
-                    size={24}
-                    style={{
-                      marginBottom: "-6px",
-                    }}
-                  />
-                );
-              }
-            },
-          },
-          {
-            accessor: "icon2",
-            title: "",
-            textAlign: "center",
-            width: "50px",
-            render: (quiz) => {
-              if (quiz.completed || quiz.inProgress) {
-                return (
-                  <Text
-                    component={Link}
-                    to={scoresheetRoute.to}
-                    // @ts-ignore (type safety unfortunately doesn't work with polymorphic links)
-                    params={{ roundNumber: quiz.tdrri }}
-                  >
-                    <SheetIcon size={24} style={{ marginBottom: "-6px" }} />
-                  </Text>
-                );
-              }
-              return (
-                <HourglassIcon
-                  color={theme.colors.gray[6]}
+  const hasThreeTeams = useMemo(
+    () => quizzes.some((quiz) => quiz.teams.length === 3),
+    [quizzes],
+  );
+
+  const columns = useMemo(() => {
+    const columns: DataTableColumn<ArrayElement<typeof quizzes>>[] = [
+      {
+        accessor: "icon",
+        title: "",
+        textAlign: "center",
+        width: "1%",
+        render: (quiz) => {
+          if (!quiz.completed && !quiz.inProgress) {
+            // Scheduled quiz
+            return (
+              <CalendarIcon
+                color={theme.colors.gray[6]}
+                size={24}
+                style={{ marginBottom: "-5px" }}
+              />
+            );
+          }
+          if (quiz.inProgress) {
+            // Quiz in progress
+            return (
+              <Text
+                component={Link}
+                to={roomStreamRoute.to}
+                // @ts-ignore (type safety unfortunately doesn't work with polymorphic links)
+                params={{ roomName: quiz.room }}
+              >
+                <MonitorPlayIcon
+                  color={theme.colors.blue[4]}
                   size={24}
-                  style={{ marginBottom: "-6px" }}
+                  style={{
+                    marginBottom: "-6px",
+                  }}
                 />
-              );
-            },
-          },
-          {
-            accessor: "time",
-            title: "Time",
-            textAlign: "center",
-            width: "250px",
-            noWrap: true,
-            hidden: !includeTime,
-            render: (quiz) => {
-              if (quiz.inProgress) {
-                return "In Progress";
-              }
-              if (quiz.completed) {
-                return "Completed";
-              }
-              return dayjs
-                .unix(Number.parseInt(quiz.time ?? ""))
-                .subtract(2, "hour")
-                .format("ddd hh:mmA (MM/DD/YY)");
-            },
-          },
-          {
-            accessor: "room",
-            title: "Room",
-            textAlign: "center",
-            width: "110px",
-            noWrap: true,
-            render: (quiz) => {
-              if (quiz.teams.some((team) => team.name === "BYE")) {
-                return "";
-              }
-              return quiz.room;
-            },
-          },
-          {
-            hidden: !showCurrentQuestionColumn,
-            accessor: "question",
-            title: "Question",
-            textAlign: "center",
-            width: "100px",
-          },
-          {
-            accessor: "teams",
-            title: "Teams",
-            textAlign: "left",
-            noWrap: true,
-            render: (quiz) => {
-              if (quiz.teams.some((team) => team.name === "Bye")) {
-                return (
-                  <Text span>
-                    <ScoresheetTeamIcon
-                      color={theme.colors.gray[6]}
-                      size={16}
-                      style={{
-                        marginRight: "0.25rem",
-                      }}
-                    />
-                    Bye round
+              </Text>
+            );
+          }
+          if (quiz.completed) {
+            // Completed quiz
+            return (
+              <CheckIcon
+                size={24}
+                style={{
+                  marginBottom: "-6px",
+                }}
+              />
+            );
+          }
+        },
+      },
+      {
+        accessor: "icon2",
+        title: "",
+        textAlign: "center",
+        width: "1%",
+        render: (quiz) => {
+          if (quiz.completed || quiz.inProgress) {
+            return (
+              <Text
+                component={Link}
+                to={scoresheetRoute.to}
+                // @ts-ignore (type safety unfortunately doesn't work with polymorphic links)
+                params={{ roundNumber: quiz.tdrri }}
+              >
+                <SheetIcon size={24} style={{ marginBottom: "-6px" }} />
+              </Text>
+            );
+          }
+          return (
+            <HourglassIcon
+              color={theme.colors.gray[6]}
+              size={24}
+              style={{ marginBottom: "-6px" }}
+            />
+          );
+        },
+      },
+      {
+        accessor: "time",
+        title: "Time",
+        textAlign: "center",
+        width: "1%",
+        noWrap: true,
+        hidden: !includeTime,
+        render: (quiz) => {
+          if (quiz.inProgress) {
+            return "In Progress";
+          }
+          if (quiz.completed) {
+            return "Completed";
+          }
+          return dayjs
+            .unix(Number.parseInt(quiz.time ?? ""))
+            .subtract(2, "hour")
+            .format("ddd hh:mmA (MM/DD/YY)");
+        },
+      },
+      {
+        accessor: "room",
+        title: "Room",
+        textAlign: "center",
+        width: "1%",
+        noWrap: true,
+        render: (quiz) => {
+          if (quiz.teams.some((team) => team.name === "BYE")) {
+            return "";
+          }
+          return quiz.room;
+        },
+      },
+      {
+        hidden: !showCurrentQuestionColumn,
+        accessor: "question",
+        title: "Question",
+        textAlign: "center",
+        width: "100px",
+      },
+    ];
+    columns.push({
+      accessor: "teams1",
+      title: "Teams",
+      textAlign: "left",
+      width: "1%", // force column not to take remaining space
+      noWrap: true,
+      render: (quiz) => {
+        if (quiz.teams.some((team) => team.name === "Bye")) {
+          return (
+            <Text span>
+              <ScoresheetTeamIcon
+                color={theme.colors.gray[6]}
+                size={16}
+                style={{
+                  marginRight: "0.25rem",
+                }}
+              />
+              Bye round
+            </Text>
+          );
+        }
+
+        const team = quiz.teams[0];
+
+        const winners: string[] = [];
+
+        const sortedByScore = [...quiz.teams].sort((a, b) => b.score - a.score);
+
+        winners.push(sortedByScore[0].name);
+
+        if (
+          sortedByScore.length > 2 &&
+          sortedByScore[0].score === sortedByScore[1].score
+        ) {
+          // Jank way to handle ties (we don't have placement information in this data)
+          winners.push(sortedByScore[1].name);
+        }
+
+        const color = getTeamColorsForTeamCount(quiz.teams.length)[0];
+
+        return (
+          <Text span>
+            <Text span fw={winners.includes(team.name) ? 700 : 500}>
+              <ScoresheetTeamIcon
+                color={color}
+                size={16}
+                style={{
+                  marginRight: "0.25rem",
+                }}
+              />
+              {primaryTeamName === team.name && (
+                <Text span fw={winners.includes(team.name) ? 700 : 500}>
+                  {team.name}
+                </Text>
+              )}
+              {primaryTeamName !== team.name && (
+                <Text
+                  component={Link}
+                  to={statGroupTeamScheduleRoute.to}
+                  fw={winners.includes(team.name) ? 700 : 500}
+                  params={{
+                    // @ts-ignore (type safety unfortunately doesn't work with polymorphic links)
+                    statGroupName: statGroupName
+                      ? statGroupName
+                      : quiz.statGroupName,
+                    teamName: team.name,
+                  }}
+                >
+                  {team.name}
+                </Text>
+              )}
+
+              {(quiz.inProgress || quiz.completed) && ` (${team.score})`}
+            </Text>
+          </Text>
+        );
+      },
+    });
+
+    columns.push({
+      accessor: "teams1.5",
+      title: "",
+      textAlign: "center",
+      width: "1%",
+      render: (quiz) => {
+        if (quiz.teams.some((team) => team.name === "Bye")) {
+          return "";
+        }
+        return "vs.";
+      },
+    });
+
+    columns.push({
+      accessor: "teams2",
+      title: "",
+      textAlign: "left",
+      width: "1%", // force column not to take remaining space
+      noWrap: true,
+      render: (quiz) => {
+        if (quiz.teams.some((team) => team.name === "Bye")) {
+          return "";
+        }
+
+        const team = quiz.teams[1];
+
+        const winners: string[] = [];
+
+        const sortedByScore = [...quiz.teams].sort((a, b) => b.score - a.score);
+
+        winners.push(sortedByScore[0].name);
+
+        if (
+          sortedByScore.length > 2 &&
+          sortedByScore[0].score === sortedByScore[1].score
+        ) {
+          // Jank way to handle ties (we don't have placement information in this data)
+          winners.push(sortedByScore[1].name);
+        }
+
+        const color = getTeamColorsForTeamCount(quiz.teams.length)[1];
+
+        return (
+          <Text span>
+            <Text span fw={winners.includes(team.name) ? 700 : 500}>
+              <ScoresheetTeamIcon
+                color={color}
+                size={16}
+                style={{
+                  marginRight: "0.25rem",
+                }}
+              />
+              {primaryTeamName === team.name && (
+                <Text span fw={winners.includes(team.name) ? 700 : 500}>
+                  {team.name}
+                </Text>
+              )}
+              {primaryTeamName !== team.name && (
+                <Text
+                  component={Link}
+                  to={statGroupTeamScheduleRoute.to}
+                  fw={winners.includes(team.name) ? 700 : 500}
+                  params={{
+                    // @ts-ignore (type safety unfortunately doesn't work with polymorphic links)
+                    statGroupName: statGroupName
+                      ? statGroupName
+                      : quiz.statGroupName,
+                    teamName: team.name,
+                  }}
+                >
+                  {team.name}
+                </Text>
+              )}
+
+              {(quiz.inProgress || quiz.completed) && ` (${team.score})`}
+            </Text>
+          </Text>
+        );
+      },
+    });
+
+    if (hasThreeTeams) {
+      columns.push({
+        accessor: "teams2.5",
+        title: "",
+        textAlign: "center",
+        width: "50px",
+        render: (quiz) => {
+          if (
+            quiz.teams.some((team) => team.name === "Bye") ||
+            quiz.teams.length < 3
+          ) {
+            return "";
+          }
+          return "Bye";
+        },
+      });
+
+      columns.push({
+        accessor: "teams3",
+        title: "",
+        textAlign: "left",
+        width: "1%", // force column not to take remaining space
+        noWrap: true,
+        render: (quiz) => {
+          if (
+            quiz.teams.some((team) => team.name === "Bye") ||
+            quiz.teams.length < 3
+          ) {
+            return "";
+          }
+
+          const team = quiz.teams[2];
+
+          const winners: string[] = [];
+
+          const sortedByScore = [...quiz.teams].sort(
+            (a, b) => b.score - a.score,
+          );
+
+          winners.push(sortedByScore[0].name);
+
+          if (
+            sortedByScore.length > 2 &&
+            sortedByScore[0].score === sortedByScore[1].score
+          ) {
+            // Jank way to handle ties (we don't have placement information in this data)
+            winners.push(sortedByScore[1].name);
+          }
+
+          const color = getTeamColorsForTeamCount(quiz.teams.length)[2];
+
+          return (
+            <Text span>
+              <Text span fw={winners.includes(team.name) ? 700 : 500}>
+                <ScoresheetTeamIcon
+                  color={color}
+                  size={16}
+                  style={{
+                    marginRight: "0.25rem",
+                  }}
+                />
+                {primaryTeamName === team.name && (
+                  <Text span fw={winners.includes(team.name) ? 700 : 500}>
+                    {team.name}
                   </Text>
-                );
-              }
-
-              const winners: string[] = [];
-              const sortedByScore = [...quiz.teams].sort(
-                (a, b) => b.score - a.score,
-              );
-
-              winners.push(sortedByScore[0].name);
-
-              if (
-                sortedByScore.length > 2 &&
-                sortedByScore[0].score === sortedByScore[1].score
-              ) {
-                // Jank way to handle ties (we don't have placement information in this data)
-                winners.push(sortedByScore[1].name);
-              }
-
-              console.log(winners);
-
-              return getTeamColorsForTeamCount(quiz.teams.length).map(
-                (color, index) => (
-                  <Text span key={color}>
-                    <Text
-                      span
-                      fw={winners.includes(quiz.teams[index].name) ? 700 : 500}
-                    >
-                      <ScoresheetTeamIcon
-                        color={color}
-                        size={16}
-                        style={{
-                          marginRight: "0.25rem",
-                        }}
-                      />
-                      {primaryTeamName === quiz.teams[index].name && (
-                        <Text
-                          span
-                          fw={
-                            winners.includes(quiz.teams[index].name) ? 700 : 500
-                          }
-                        >
-                          {quiz.teams[index].name}
-                        </Text>
-                      )}
-                      {primaryTeamName !== quiz.teams[index].name && (
-                        <Text
-                          component={Link}
-                          to={statGroupTeamScheduleRoute.to}
-                          fw={
-                            winners.includes(quiz.teams[index].name) ? 700 : 500
-                          }
-                          params={{
-                            // @ts-ignore (type safety unfortunately doesn't work with polymorphic links)
-                            statGroupName: statGroupName
-                              ? statGroupName
-                              : quiz.statGroupName,
-                            teamName: quiz.teams[index].name,
-                          }}
-                        >
-                          {quiz.teams[index].name}
-                        </Text>
-                      )}
-
-                      {(quiz.inProgress || quiz.completed) &&
-                        ` (${quiz.teams[index].score})`}
-                    </Text>
-                    {index < quiz.teams.length - 1 ? " vs. " : ""}
+                )}
+                {primaryTeamName !== team.name && (
+                  <Text
+                    component={Link}
+                    to={statGroupTeamScheduleRoute.to}
+                    fw={winners.includes(team.name) ? 700 : 500}
+                    params={{
+                      // @ts-ignore (type safety unfortunately doesn't work with polymorphic links)
+                      statGroupName: statGroupName
+                        ? statGroupName
+                        : quiz.statGroupName,
+                      teamName: team.name,
+                    }}
+                  >
+                    {team.name}
                   </Text>
-                ),
-              );
-            },
-          },
-          {
-            accessor: "round",
-            title: "Round",
-            textAlign: "center",
-            width: "110px",
-            hidden: !showRoundColumn,
-          },
-        ]}
+                )}
+
+                {(quiz.inProgress || quiz.completed) && ` (${team.score})`}
+              </Text>
+            </Text>
+          );
+        },
+      });
+    }
+
+    columns.push({
+      accessor: "round",
+      title: "Round",
+      textAlign: "center",
+      width: "110px",
+      hidden: !showRoundColumn,
+    });
+    return columns;
+  }, [
+    statGroupName,
+    primaryTeamName,
+    showRoundColumn,
+    includeTime,
+    showCurrentQuestionColumn,
+    hasThreeTeams,
+  ]);
+
+  return (
+    <Box
+      mb="md"
+      mih="50vh"
+      maw="75em"
+      mx="auto"
+      sx={(theme) => ({
+        marginBottom: theme.spacing.md,
+        minHeight: "50vh",
+        maxWidth: hasThreeTeams ? "100%" : "75em",
+        marginLeft: "auto",
+        marginRight: "auto",
+      })}
+    >
+      <DataTable
+        columns={columns}
         records={quizzes.filter((quiz) => quiz.teams.length > 1)}
         striped
         withRowBorders
